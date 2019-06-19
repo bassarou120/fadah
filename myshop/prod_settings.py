28f9c560d55896f69ae154b0b6da6b76aece3550
yacoubou bassarou <yacouboubassarou@gmail.com>
2019-06-19 23:16:30 +0100

    Configer postgresSl

--- myshop/prod_settings.py
+++ myshop/prod_settings.py
@@ -1,3 +1,5 @@
+
+import dj_database_url
 from .settings import *
 
 
@@ -7,3 +9,5 @@ TEMPLATE_DEBUG=False
 ALLOWED_HOSTS = ['fadahtech.herokuapp.com']
 
 SECRET_KEY = 'y_3nim**kz(xxdjvdx0l!ld(s@mp3eng-b3d^em$**v2s43pt0'
+
+ DATABASES['default'] = dj_database_url.config()
