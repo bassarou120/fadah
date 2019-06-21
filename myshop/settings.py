0a0f421249026d392454f3621650937bfa29da17
yacoubou bassarou <yacouboubassarou@gmail.com>
2019-06-21 19:25:46 +0100

    Initial commit

--- myshop/settings.py
+++ myshop/settings.py
@@ -153,7 +153,7 @@ STATIC_ROOT=os.path.join(BASE_DIR, 'staticfiles')
 STATIC_URL = '/static/'
 
 MEDIA_URL = '/media/'
-MEDIA_ROOT = os.path.join(BASE_DIR, 'products/')
+MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
 
 CART_SESSION_ID = 'cart'
 
